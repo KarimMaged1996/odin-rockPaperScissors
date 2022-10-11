6358f2e0967a9ed7a2e32b6c17708aae450278cf
KarimMaged <karim.maged2020@yahoo.com>
2022-10-11 18:46:02 +0200

    Add comments to the JavaScript file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//global variables to store the player and computer score
 let compScore=0;
 let playerScore=0;
 
+//a function that will generate random choice between rock paper and scissors
 function computerChoice(){
 let compChoice='';
 let random=Math.floor(Math.random()*3);
 random === 0 ? compChoice='Rock' : random ===1 ? compChoice='Paper' : compChoice ='Scissors';
 return compChoice;}
 
+// the main function of the game
 function game(){
 let compChoice=computerChoice();
 let playerChoice= prompt('your turn Choose "Rock, Paper or Scissors"').toLowerCase();
@@ -46,6 +49,7 @@ else if(compChoice.toLowerCase()==='scissors' && playerChoice==='scissors'){
 }
 }
 
+// finally a function to play the game() function for only 5 rounds and then report the score
 function round(){
 for(let i=0; i<5;i++){
 console.log(game());}
